99a1cf9c7cf2eeaa33c1053fe35d1ca49fa981d5
OsamaALhamoud <osama.alhamoud@student.rocva.nl>
2021-05-25 13:56:31 +0100

    voldaan

---
+++ 01-Challenge/Taak02-Ontwerpen/index/javascript.js
@@ -0,0 +1,21 @@
+let persoon ={
+    voornaam: prompt ("Wat is je voornaam?"),
+    age: prompt ("wat is je leeftijd"),
+}
+
+
+
+document.getElementById("naam").innerHTML = "Welkom "+ persoon.voornaam;
+
+
+document.querySelector("body").style.backgroundColor = "blue";
+
+if (persoon.age >18)
+
+{
+    document.getElementById("naam").style.backgroundColor = "yellow";
+}
+
+else {
+    document.getElementById("naam").style.backgroundColor = "red";
+}
