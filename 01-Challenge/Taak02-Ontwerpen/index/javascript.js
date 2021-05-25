d2e2567b327e96d9ae307c2a3236edf3d51f2a4b
OsamaALhamoud <osama.alhamoud@student.rocva.nl>
2021-05-25 14:46:46 +0100

    feedback  toegevoegd

--- 01-Challenge/Taak02-Ontwerpen/index/javascript.js
+++ 01-Challenge/Taak02-Ontwerpen/index/javascript.js
@@ -1,21 +1,21 @@
-let persoon ={
-    voornaam: prompt ("Wat is je voornaam?"),
-    age: prompt ("wat is je leeftijd"),
-}
+// let persoon ={
+//     voornaam: prompt ("Wat is je voornaam?"),
+//     age: prompt ("wat is je leeftijd"),
+// }
 
 
 
-document.getElementById("naam").innerHTML = "Welkom "+ persoon.voornaam;
+// document.getElementById("naam").innerHTML = "Welkom "+ persoon.voornaam + " ..." ;
 
 
-document.querySelector("body").style.backgroundColor = "blue";
+// document.querySelector("body").style.backgroundColor = "blue";
 
-if (persoon.age >18)
+// if (persoon.age >18)
 
-{
-    document.getElementById("naam").style.backgroundColor = "yellow";
-}
+// {
+//     document.getElementById("naam").style.backgroundColor = "yellow";
+// }
 
-else {
-    document.getElementById("naam").style.backgroundColor = "red";
-}
+// else {
+//     document.getElementById("naam").style.backgroundColor = "red";
+// }
--- 01-Challenge/Taak05-Presenteren/student-feedback.md
+++ 01-Challenge/Taak05-Presenteren/student-feedback.md
@@ -5,6 +5,13 @@
 
 ## Feedback.
 
-- ...
-- ...
-- ...
+- ...Joëny Ogenia
+ Ik vind het heel mooi goed beizg 
+
+- ...Denzel van Windt
+
+Goed goed goed
+
+- ...Chesron
+
+Wat mooi zich u bent beter dan de andere 
